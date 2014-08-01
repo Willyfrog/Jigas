9275e0b056856e9329c4592e4589116f8921f8f3
Guillermo Vaya <guillermo.vaya@gigas.com>
2014-08-01 09:43:31 +0200

    Fixed duckduck plugin

--- commands/duckduck.js
+++ commands/duckduck.js
@@ -7,34 +7,36 @@ const dd_url = "http://api.duckduckgo.com";
 module.exports = {
   duckduck: function (bot, command) {
     if (_.isEmpty(command.text)) {
-      bot.say("What do you want to search?");
+      bot.say(command.origin, "What do you want to search?");
     } else {
       var query = {q:command.text.split().join('+'), format:"json", t:"IRC Bot"},
           had_result = false;
-      console.log("Asking ddg with query %s", query.q);
+      //console.log("Asking ddg with query %s", query.q);
       
       request({uri: dd_url, qs: query, json: true}, function (error, response, result) {
         if (error) {
-          bot.say(command.to, "Sorry, but duckduckgo gave me an error: " + error);
+          bot.say(command.origin, "Sorry, but duckduckgo gave me an error: " + error);
         }
         if (response.status >= 400) {
-          bot.say(command.to, "Sorry, but duckduckgo gave me an error " + response.status);
+          bot.say(command.origin, "Sorry, but duckduckgo gave me an error " + response.status);
         }
-        console.log("got result: %s", util.inspect(result));
-        if (typeof result.AbstractText !== "undefined") {
-          bot.say(command.to, "Duckduck said: " + result.AbstractText);
+        //console.log("got result: %s", util.inspect(result));
+        if (typeof result.AbstractText !== "undefined" && !_.isEmpty(result.AbstractText)) {
+          bot.say(command.origin, "Duckduck said: " + result.AbstractText);
           had_result = true;
         }
-        if (typeof result.AbstractUrl !== "undefined") {
-          bot.say(command.to, "Url: " + result.AbstractUrl);
+        if (typeof result.AbstractUrl !== "undefined" && !_.isEmpty(result.AbstractUrl)) {
+          bot.say(command.origin, "Url: " + result.AbstractUrl);
           had_result = true;
         } 
         if (!had_result){
-          bot.say(command.to, "No instant result found");
+          bot.say(command.origin, "No instant result found");
         }
 
         _.forEach(result.RelatedTopics, function (related) {
-          bot.say("Related: " + related.Text + " => " + related.FirstURL);
+          if (!_.isEmpty(related.Text)) {
+            bot.say(command.origin, "Related: " + related.Text + " => " + related.FirstURL);
+          }
         });
       });
     }
